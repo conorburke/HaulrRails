10e68b0a7a94b4f290da89d6d81aa9de319a50cd
conorburke <cjburke89@gmail.com>
2016-12-16 14:17:41 -0800

    create and test car validation and association tests

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,3 +1,7 @@
 class Car < ApplicationRecord
   belongs_to :driver, class_name: :User
+
+  validates :driver_id, :make, :model, :year, :color, presence: true
+
+  validates :approved, :default_car, numericality: {only_integer: true, greater_than_or_equal_to: 0 }
 end
--- spec/models/car_spec.rb
+++ spec/models/car_spec.rb
@@ -1,5 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Car, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:driver) {User.create(first_name: "Conor", last_name: "Burke", email: "cjburke89@gmail.com", password_digest: "1234", phone: "717-608-8969", zipcode: "92102", status: "driver", active_driver: 1 )}
+  let(:car) {Car.create(driver_id: driver.id, make: "Toyota", model: "Tacoma", year: "2007", color: "red")}
+  
+  describe 'associations' do 
+    it { should belong_to(:driver) }
+  end
+
+  describe 'validations' do 
+    it { is_expected.to validate_presence_of(:driver_id) }
+    it { is_expected.to validate_presence_of(:make) }
+    it { is_expected.to validate_presence_of(:model) }
+    it { is_expected.to validate_presence_of(:year) }
+    it { is_expected.to validate_presence_of(:color) }
+    it { expect(car.approved).to be_kind_of(Integer)}
+    it { expect(car.default_car).to be_kind_of(Integer)}
+    it { expect(car.approved).to be >= 0}
+    it { expect(car.default_car).to be >= 0}
+  end
+
+  # pending "add some examples to (or delete) #{__FILE__}"
 end
