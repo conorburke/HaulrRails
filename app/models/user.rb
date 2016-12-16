0e16044f69f7c248c07c31d2dc1c1d2d05d23dff
Chinh Nguyen & Benjamin Kwok & Tae Yun <pair+chqnguyen.kwokhuen.yuntsj@devbootcamp.com>
2016-12-15 17:47:15 -0800

    Add associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  # Associations:
+  has_many :cars, foreign_key: :driver_id
+  has_many :credit_cards
+  has_many :rides, foreign_key: :rider_id
+  has_many :drives, class_name: :Ride, foreign_key: :driver_id
+  has_secure_password
+
+  # Validations:
 end
