f3290b1f1147c9ac8209f6602e124f9e1c85e8d4
conorburke <cjburke89@gmail.com>
2016-12-15 16:34:56 -0800

    adjust spec file for travis

--- spec/models/blank_spec.rb
+++ spec/models/blank_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-it ('is a blank test') do 
-  expect('true').to eq('true')
+Rspec.describe 'it is a simple test' do
+  it 'is a blank test' do 
+    expect('true').to eq 'true'
+  end
 end
