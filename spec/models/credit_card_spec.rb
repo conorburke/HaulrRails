42aa3d738eb7071294d7633f17916a496b213845
Kelsey Edelstein <email": null>
2016-12-16 14:55:16 -0800

    Add regex validation for card number

--- spec/models/credit_card_spec.rb
+++ spec/models/credit_card_spec.rb
@@ -4,13 +4,13 @@ RSpec.describe CreditCard, type: :model do
   # pending "add some examples to (or delete) #{__FILE__}"
   context 'credit card is valid' do
     creditcard = CreditCard.new(user_id: 1,
-                                card_number: "1234123412341234",
+                                card_number: "1234-1234-1234-1234",
                                 card_name: "Chase",
                                 expiration_date: "01-2018",
                                 security_code: "123")
 
     it 'has a number' do
-      expect(creditcard).to have_attributes(card_number: "1234123412341234")
+      expect(creditcard).to have_attributes(card_number: "1234-1234-1234-1234")
     end
 
     it 'has a name' do
@@ -38,13 +38,18 @@ RSpec.describe CreditCard, type: :model do
   context 'active record' do
     context 'validations' do
       subject { CreditCard.new(user_id: 1,
-                                card_number: "1234123412341234",
+                                card_number: "1234-1234-1234-1234",
                                 card_name: "Chase",
                                 expiration_date: "01-2018",
                                 security_code: "123") }
-      it { should validate_presence_of(:card_number) }
-      it { should validate_uniqueness_of(:card_number).ignoring_case_sensitivity }
-      it { should validate_length_of(:card_number).is_equal_to(16) }
+      context 'card_number' do
+        it { should validate_presence_of(:card_number) }
+        it { should validate_uniqueness_of(:card_number).ignoring_case_sensitivity }
+        it { should validate_length_of(:card_number).is_equal_to(19) }
+
+        it { should allow_value('1234-1234-1234-1234').for(:card_number) }
+        it { should_not allow_value('1234-1234-abcd-abcd').for(:card_number) }
+      end
 
       it { should validate_presence_of(:user_id) }
       it { should validate_presence_of(:card_name) }
