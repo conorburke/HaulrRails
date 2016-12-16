a0838213798ca86385455f00a5944e1848359d49
Chinh Nguyen & Benjamin Kwok & Tae Yun <pair+chqnguyen.kwokhuen.yuntsj@devbootcamp.com>
2016-12-16 14:40:06 -0800

    Add validations for rides

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -3,4 +3,14 @@ class Ride < ApplicationRecord
   belongs_to :rider, class_name: :User
   belongs_to :driver, class_name: :User
 
+  # Validations:
+
+  STATUS_LIST = %w(contacting waiting riding complete)
+
+  validates :rider, presence: true
+  validates :status, inclusion: { in: STATUS_LIST, message: "%{value} is not a valid status" }
+  validates :driver_score, :rider_score, numericality: { integer_only: true,
+                                                         greater_than_or_equal_to: 1,
+                                                         less_than_or_equal_to: 5 }
+
 end
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Ride, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Associations" do
+    it 'belongs to a rider' do
+    end
+
+    it 'belongs to a driver' do
+    end
+  end
+
+  describe "Validations" do
+  end
 end
