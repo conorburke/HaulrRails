4dcb55388793f4a2c65397dd312a61fffa8ead56
Patrick Anderson <Patrick.Anderson.000@gmail.com>
2016-12-15 18:15:29 -0800

    ride validations and tests

--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Ride, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should belong_to(:rider) }
+  it { should belong_to(:driver) }
+
+  it { is_expected.to validate_presence_of(:status) }
+  it { is_expected.to validate_presence_of(:rider) }
+  it { is_expected.to validate_presence_of(:driver) }
 end
